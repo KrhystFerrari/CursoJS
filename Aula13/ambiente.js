7d1defe974c38f42cc523c298eba77aae56c47a3
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-09-14 16:16:02 -0300

    Create ambiente.js

---
+++ Aula13/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+do {  //Exemplo de repetição com teste lógico no fim (do)
+    console.log(`Passo ${c} `)
+    c += 1
+} while (c <= 6)
+
+
+
+
+
+
+/*
+var c = 1
+while (c <= 15) {  //Exemplo de repetição com teste lógico no ínicio (while)
+    console.log(`Passo ${c} `)
+    c += 1
+}
+*/
