12171262facf82d8dff492d364923b595a8471d5
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-09-14 00:24:23 -0300

    Create Ex008.js

---
+++ Aula11/Ex008.js
@@ -0,0 +1,7 @@
+var vel = 100
+
+console.log(`A velocidade do seu carro é ${vel}Km/h `)
+if (vel > 60) {  //Condição simples.
+    console.log('Você ultrapassou o limite de velocidade. MULTADO! ')
+}
+console.log('Dirija sempre com cinto de segurança! ')
