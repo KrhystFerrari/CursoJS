972521c28ee4793b600968a35e9b07695cdc52d1
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-09-14 16:44:46 -0300

    Create ambiente.js

---
+++ Aula14/ambiente.js
@@ -0,0 +1,24 @@
+console.log('Vai começar... ')
+for (var c = 1; c <= 10; c += 1) {  //Exemplo de repetição usando o 'for'
+    console.log(c)
+}
+console.log('FIM! ')
+
+
+
+
+/*
+-Exemplos com while;
+
+var c = 1
+while (c <= 10) { //Exemplo de repetição com teste lógico no ínicio.
+    console.log(`Passo ${c} `)
+    c += 1
+}
+
+var c = 1
+do {  //Exemplo de repetição com teste lógico no fim (do)
+    console.log(`Passo ${c} `)
+    c += 1
+} while (c <= 6)
+*/
