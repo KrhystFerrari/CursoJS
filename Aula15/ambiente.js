41fecb59e5a370f0d33efd378f5e2929d9bfc6e9
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-09-14 21:30:46 -0300

    Update ambiente.js

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -1,6 +1,13 @@
 var num = [5, 8, 2, 9, 3]  //Array ou vetor
-num.sort()
 num.push(1)
+num.sort()
 console.log(num)
 console.log(`O vetor tem ${num.length} posições. `)
 console.log(`O primeiro valor do vetor é ${num[0]} `)
+
+var pos = num.indexOf(8)  //Caso retorne '-1' quer dizer q o valor não foi encontrado
+if (pos == -1) {
+    console.log('O valor não foi encontrado! ')
+} else {
+    console.log(`O valor 8 está na posição ${pos} `)
+}
