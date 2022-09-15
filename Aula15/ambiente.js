28f6625db3e37b4600114bcb604526befbbc20d7
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-09-14 21:02:43 -0300

    Create ambiente.js

---
+++ Aula15/ambiente.js
@@ -0,0 +1,6 @@
+var num = [5, 8, 2, 9, 3]  //Array ou vetor
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições. `)
+console.log(`O primeiro valor do vetor é ${num[0]} `)
